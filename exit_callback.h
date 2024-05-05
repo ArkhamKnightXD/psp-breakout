cf4f68081ad487544d72ae63532889233c36b60e
knight <karvin_jimenez@hotmail.com>
2024-05-05 13:06:25 -0400

    extra changes.

--- exit_callback.h
+++ exit_callback.h
@@ -3,23 +3,13 @@
 
 #include <pspkernel.h>
 
-static inline int exit_callback(int arg1, int arg2, void *common) {
-    sceKernelExitGame();
-    return 0;
-}
+// Exit callback function
+int exit_callback(int arg1, int arg2, void *common);
 
-static inline int callback_thread(SceSize args, void *argp) {
-    int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-    sceKernelRegisterExitCallback(cbid);
-    sceKernelSleepThreadCB();
-    return 0;
-}
+// Callback thread function
+int callback_thread(SceSize args, void *argp);
 
-static inline int setup_callbacks(void) {
-    int thid = sceKernelCreateThread("update_thread", callback_thread, 0x11, 0xFA0, 0, 0);
-    if(thid >= 0)
-        sceKernelStartThread(thid, 0, 0);
-    return thid;
-}
+// Function to set up exit callbacks
+int setup_callbacks(void);
 
 #endif /* EXIT_CALLBACK_H */
--- psp_graphics.h
+++ psp_graphics.h
@@ -1,38 +1,19 @@
 #ifndef PSP_GRAPHICS_H
 #define PSP_GRAPHICS_H
 
-#include <pspgu.h>
- 
+//The buffer is 512, because the buffer size needs to be the closest power of 2 to the psp resolution,
+// and because has to be bigger than 480, is 512 instead of 256  
 #define BUFFER_WIDTH 512
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 272
 
-static inline void initGu(char *list) {
+#include <pspgu.h>
+#include <pspdisplay.h>
 
-    sceGuInit();
+//boilerplate code for setting up the psp graphics.
+void initGu(char *list);
 
-    sceGuStart(GU_DIRECT, list);
-    sceGuDrawBuffer(GU_PSM_8888, (void *)0, BUFFER_WIDTH);
-    sceGuDispBuffer(SCREEN_WIDTH, SCREEN_HEIGHT, (void *)0x88000, BUFFER_WIDTH);
-    sceGuDepthBuffer((void *)0x110000, BUFFER_WIDTH);
-
-    sceGuOffset(2048 - (SCREEN_WIDTH / 2), 2048 - (SCREEN_HEIGHT / 2));
-    sceGuViewport(2048, 2048, SCREEN_WIDTH, SCREEN_HEIGHT);
-    sceGuEnable(GU_SCISSOR_TEST);
-    sceGuScissor(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    sceGuDepthRange(65535, 0); 
-
-    sceGuDepthFunc(GU_GEQUAL); 
-    sceGuEnable(GU_DEPTH_TEST); 
-    sceGuFinish();
-    sceGuDisplay(GU_TRUE);
-}
-
-static inline void endGu()
-{
-    sceGuDisplay(GU_FALSE);
-    sceGuTerm();
-}
+// closing psp graphics.
+void endGu();
 
 #endif /* PSP_GRAPHICS_H */
